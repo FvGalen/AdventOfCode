e9f620fe712ccadc240e90a76903effb702c9470
FvGalen <f.vangalen@live.nl>
2024-12-02 21:17:22 +0100

    First solution and proof of concept

--- AdventOfCode.Application/Solution/2024/Day01/Solution.cs
+++ AdventOfCode.Application/Solution/2024/Day01/Solution.cs
@@ -9,7 +9,16 @@ public class Solution : ISolution
 {
     public AdventResult<object> PartOne()
     {
-        return Failure(new AdventError(0001, "Part one not implemented"));
+        var input = new StreamReader($"{AppDomain.CurrentDomain.BaseDirectory}/Solution/2024/Day01/Input.txt").ReadToEnd();
+
+        var column1 = input.Split('\n').Select(l => Int32.Parse(l.Split("   ")[0])).Order();
+        var column2 = input.Split('\n').Select(l => Int32.Parse(l.Split("   ")[1])).Order();
+
+        var answer = Enumerable.Zip(column1, column1)
+                               .Select(p => Math.Abs(p.First - p.Second))
+                               .Sum();
+
+        return Success(answer);
     }
 
     public AdventResult<object> PartTwo()
